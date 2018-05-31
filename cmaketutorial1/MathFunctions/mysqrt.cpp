ebf02c2d30b325d68f11caa5e784e7bb39d95aa4
nguyenviettien13 <tiennv000@gmail.com>
2018-05-31 08:29:39 +0700

    huong dan cach su dung option

---
+++ cmaketutorial1/MathFunctions/mysqrt.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <math.h>
+
+double mysqrt(double value)
+{
+	std::cout << "Ham can bac 2 cua toi:" <<std::endl;
+	return sqrt(value);
+}
---
+++ cmaketutorial1/tutorial.cpp
@@ -0,0 +1,29 @@
+// A simple program that computes the square root of a number
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "TutorialConfig.h"
+#ifdef USE_MYMATH
+#include "mathfunctions.h"
+#endif
+ 
+int main (int argc, char *argv[])
+{
+  if (argc < 2)
+    {
+    fprintf(stdout,"Usage: %s number\n",argv[0]);
+    //return 1;
+    }
+ 
+  double inputValue = atof(argv[1]);
+ 
+#ifdef USE_MYMATH
+  double outputValue = mysqrt(inputValue);
+#else
+  double outputValue = sqrt(inputValue);
+#endif
+ 
+  fprintf(stdout,"The square root of %g is %g\n",
+          inputValue, outputValue);
+  return 0;
+}
